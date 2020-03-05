b97bc2945f6dc89df84cfc4d2ebb21db598e8097
rajkiran-git1 <61458276+rajkiran-git1@users.noreply.github.com>
2020-03-05 11:41:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,31 @@ provider "aws" {
   region = var.AWS_REGION
 }
 
+##################################################################
+# Data sources to get VPC, subnet, security group and AMI details
+##################################################################
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "vpc-075ce6610f7774b07	"
+}
+
+
+module "security_group" {
+  source  = "../../../modules/network/securitygroup"
+  name        = "ECP-APP Security Group"
+  description = "Security group for example usage with EC2 instance"
+  vpc_id      = "vpc-075ce6610f7774b07"
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
+}
+  
+  #resource "aws_key_pair" "generated_key" {
+#  key_name   = var.key_name
+#  public_key = tls_private_key.sshkey.public_key_openssh
+  
+  
